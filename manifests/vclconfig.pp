560206e9c0cee2fa004785e7a203e02f8173ed6d
Jean-François Roche <jfroche@affinitic.be>
2021-10-13 16:48:47 +0200

    feat: split varnish configs by project

--- manifests/vclconfig.pp
+++ manifests/vclconfig.pp
@@ -14,7 +14,7 @@
 #   host aliases to match before using this config
 #
 define varnish::vclconfig ($backend, $vcl_config='default', $ensure='present',
-  $aliases=[], $order='50') {
+  $aliases=[], $order='50', $project='iasmartweb4') {
 
     if is_string($aliases) {
       $alias_list = split($aliases, ',')
@@ -46,16 +46,17 @@ define varnish::vclconfig ($backend, $vcl_config='default', $ensure='present',
     }
 
     file {
-        "/etc/varnish/sites/${name}.vcl":
-            ensure  => $ensure,
-            content => template("varnish/${vcl_config}_vcl_config.erb"),
-            notify  => Service[$::varnish::service_name],
-            require => [Package[$::varnish::package_name], File['/etc/varnish/sites']],
+      "/etc/varnish/projects/${project}/${name}.vcl":
+        ensure  => $ensure,
+        content => template("varnish/${vcl_config}_vcl_config.erb"),
+        notify  => Service[$::varnish::service_name],
+        require => [Package[$::varnish::package_name], File['/etc/varnish/sites']],
     }
+
     if $ensure == 'present' {
       concat::fragment {"${name}_varnish_vcl_config":
-        target  => '/etc/varnish/sites.vcl',
-        content => "include \"/etc/varnish/sites/${name}.vcl\";\n",
+        target  => "/etc/varnish/projects/${project}/sites.vcl",
+        content => "include \"/etc/varnish/projects/${project}/${name}.vcl\";\n",
         order   => $order,
         notify  => Service[$::varnish::service_name],
         require => Package[$::varnish::package_name],
